d1f864bea04a11ebdf8ca3be924386f5fec24895
giangnn-bkace <giangnn.bkace@gmail.com>
2018-07-20 14:08:45 +0900

    finetune_cyclic

--- finetune_cyclic_lr.py
+++ finetune_cyclic_lr.py
@@ -281,7 +281,7 @@ def main(dataset='ucf101', mode='rgb', split=1):
                 logging.info('Epoch%d, test accuracy: %.3f' %
                              (train_data.epoch_completed, accuracy))
                 # saving the best params in test set
-                saver2.save(sess, os.path.join(log_dir, test_data.name+'_'+train_data.mode)
+                saver2.save(sess, os.path.join(log_dir, test_data.name+'_'+train_data.mode))
                 sess.run(train_init_op)
     train_writer.close()
     sess.close()
